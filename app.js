6ebf9c6357ee3a987d58755eb7cf03c48820d4f5
TarunShyam001 <shyamtarun2001@gmail.com>
2024-08-13 23:07:20 +0530

    Task02 is successfully completed

--- app.js
+++ app.js
@@ -14,14 +14,6 @@ app.set('view engine', 'ejs');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-database.execute('SELECT * FROM products;')
-.then(result => {
-    console.log(result[0]);
-})
-.catch(err => {
-    console.log('Database query error:', err);
-});
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
