334fb1d0f133d16f36166f6ceb33b7407f6f0370
TarunShyam001 <shyamtarun2001@gmail.com>
2024-08-13 23:06:32 +0530

    Task is updated

--- app.js
+++ app.js
@@ -14,13 +14,13 @@ app.set('view engine', 'ejs');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-// database.execute('SELECT * FROM products;')
-// .then(result => {
-//     console.log(result[0]);
-// })
-// .catch(err => {
-//     console.log('Database query error:', err);
-// });
+database.execute('SELECT * FROM products;')
+.then(result => {
+    console.log(result[0]);
+})
+.catch(err => {
+    console.log('Database query error:', err);
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
